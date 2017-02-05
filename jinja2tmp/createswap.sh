c2fbb2c95974c590460d88a469f5887f4c0ba2e2
chingchong <norbuurgen@gmail.com>
2017-02-05 11:38:47 +0545

    improved create swap script

--- jinja2tmp/createswap.sh
+++ jinja2tmp/createswap.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-mkdir /swp
-fallocate -l 512M /swp/.swapfile
-chmod 600 /swp/.swapfile
-mkswap /swp/.swapfile
-swapon /swp/.swapfile
-echo "/swp/.swapfile none swap defaults 0 0" >> /etc/fstab
+swapon -s | grep "/swp/.swapfile" > /dev/null 2>&1
+val=$?
+if [ $val -gt 0 ];
+then
+    mkdir /swp
+    fallocate -l 512M /swp/.swapfile
+    chmod 600 /swp/.swapfile
+    mkswap /swp/.swapfile
+    swapon /swp/.swapfile
+    echo "/swp/.swapfile none swap defaults 0 0" >> /etc/fstab
+else
+  :
+fi
+
